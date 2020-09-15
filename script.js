571d21f08bdaef59b4b49ce0fb66fdce072c205b
Audrey Andoy <audreylandoy@gmail.com>
2020-09-15 23:14:55 +0000

    Started calculator, displayed buttons, and added some logic for clicking.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-console.log("hi")
+var num1;
+var num2;
 
 function add(n1, n2) {
     var sum = n1 + n2
@@ -8,4 +9,27 @@ function add(n1, n2) {
 }
 
 add(2,4)
+
+function clicked(num) {
+  
+
+    if (num1 == null) {
+        num1 = num;
+        console.log("num1", num1);
+    }
+
+    else if (num1 !== null) {
+        num2 = num;
+        console.log("num2", num2)
+    }
+
+    // document.getElementsByClassName("num").innerHTML = "You clicked me!";
+
+    // num1 = 1;
+    // console.log(num1)
+
+    // console.log(document.getElementsByClassName("num")[2].innerHTML)
+    
+}
+
 
