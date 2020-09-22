6e24a4f5d782b186b7acd43dd6136e168f97d51d
Audrey Andoy <audreylandoy@gmail.com>
2020-09-22 22:23:37 +0000

    Homework for L4: add HTML buttons for operators (+, -, /, *) and buttons to clear and add decimals

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 var num1;
 var num2;
 
-function add(n1, n2) {
-    var sum = n1 + n2
+function add() {
+    var sum = num1 + num2
     console.log(sum);
+
+    num1 = null;
+    num2 = null;
 
     document.getElementById("sum").innerHTML = sum;
 }
 
-add(2,4)
-
 function clicked(num) {
-  
-
-    if (num1 == null) {
+    if (num1 == null) 
+    {
         num1 = num;
         console.log("num1", num1);
     }
 
-    else if (num1 !== null) {
+    else if (num1 !== null && num2 == null){
         num2 = num;
         console.log("num2", num2)
     }
+}
+  
+  
 
+    // else if (num1 !== null && num2 !== null) {
+    //     num1 = num;
+    //     console.log("num1", num1)
+    // }
     // document.getElementsByClassName("num").innerHTML = "You clicked me!";
 
     // num1 = 1;
@@ -30,6 +37,6 @@ function clicked(num) {
 
     // console.log(document.getElementsByClassName("num")[2].innerHTML)
     
-}
+
 
 
